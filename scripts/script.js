42bd1ed313f56e3acc9ab62e29d5788507f7f07f
deveney1 <90417686+deveney1@users.noreply.github.com>
2023-01-05 22:20:18 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+var deMenuButton = document.querySelector("header>button");
+var deNav = document.querySelector("header nav:first-of-type");
+
+var deSluitButton = document.querySelector("header nav button:first-of-type")
+
+deMenuButton.addEventListener("click", toggleMenu);
+deSluitButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	deNav.classList.toggle("open");
+}
